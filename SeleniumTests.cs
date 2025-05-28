601ce00ff1e6eb4870b2db113118ceb39c9972ab
arishirfan98 <arishirfan98@gmail.com>
2025-05-29 00:56:28 +0500

    Initial commit: Add MSTest Selenium data-driven tests

--- SeleniumTests.cs
+++ SeleniumTests.cs
@@ -12,29 +12,11 @@ using System.Threading;
 namespace DataDrivenSeleniumTests
 {
     [TestClass]
-    public class SeleniumTests : ExtentReport
+    public class SeleniumTests
     {
         public TestContext TestContext { get; set; }
 
-        [TestInitialize]
-        public void Initialize()
-        {
-
-            inti();
-            LogReport("Testing");
-            driver.Manage().Window.Maximize();
-
-        }
-
-        [TestCleanup]
-        public void Cleanup()
-        {
-
-            Thread.Sleep(3000);
-            extentReports.Flush();
-            driver.Close();
-
-        }
+        
 
         [TestMethod]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\Data.csv", "Data#csv", DataAccessMethod.Sequential)]
@@ -73,10 +55,9 @@ namespace DataDrivenSeleniumTests
             options.AddArguments("start-maximized", "incognito");
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl(url);
-            Write(By.Id("username"), username, "Enter your username");
-            Write(By.Id("password"), password, "Enter your password");
-            //driver.FindElement(By.Id("username")).SendKeys(username);
-            //driver.FindElement(By.Id("password")).SendKeys(password);
+
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Id("password")).SendKeys(password);
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             IWebElement loginButton = wait.Until(Driver =>
             {
@@ -96,31 +77,26 @@ namespace DataDrivenSeleniumTests
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\Data.csv", "Data#csv", DataAccessMethod.Sequential)]
         public void TestCase_DataDriven_CSV_003()
         {
-            exParentTest = extentReports.CreateTest("Project Test");
-
-            exChildTest = exParentTest.CreateNode("Login");
+            
             string url = TestContext.DataRow["url"].ToString();
             string username = TestContext.DataRow["username"].ToString();
             string password = TestContext.DataRow["password"].ToString();
-            //EdgeOptions options = new EdgeOptions();
-            //options.AddArguments("start-maximized", "incognito");
-            //IWebDriver driver = new EdgeDriver(options);
-            driver.Navigate().GoToUrl(url);
-            //driver.FindElement(By.Id("username")).SendKeys(username);
-            //driver.FindElement(By.Id("password")).SendKeys(password);
+            EdgeOptions options = new EdgeOptions();
+            options.AddArguments("start-maximized", "incognito");
+            IWebDriver driver = new EdgeDriver(options);
             
 
-            Write(By.Id("username"), username, "Enter your username");
+            driver.Navigate().GoToUrl(url);
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Id("password")).SendKeys(password);
 
-            Write(By.Id("password"), password, "Enter your password");
-            //Click(By.CssSelector("#login > button"), "Click on submit button");
+            
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             IWebElement loginButton = wait.Until(Driver =>
             {
                 var element = driver.FindElement(By.CssSelector("#login > button"));
                 return (element.Displayed && element.Enabled) ? element : null;
-                Thread.Sleep(4000);
-                TakeScreenshot(Status.Pass,"Click on button");
+                
             });
 
             // Scroll into view
@@ -131,7 +107,9 @@ namespace DataDrivenSeleniumTests
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", loginButton);
             driver.Quit();
         }
+        
         }
     }
+
 
 
